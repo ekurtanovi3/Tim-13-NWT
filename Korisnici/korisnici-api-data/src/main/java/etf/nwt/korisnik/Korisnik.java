f38b9882ef358c9dc6e41b6c22b6ef715c20c19b
arzama <arza_1993@hotmail.com>
2017-04-04 21:13:48 +0200

    KorisnikRepository

---
+++ Korisnici/korisnici-api-data/src/main/java/etf/nwt/korisnik/Korisnik.java
@@ -0,0 +1,58 @@
+package etf.nwt.korisnik;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Korisnik {
+	@Id
+	@GeneratedValue
+	private int Id; 
+	private String Ime;
+	private String Prezime;
+	private String Username;
+	private String Password;
+	
+	public Korisnik() {}
+	
+	public Korisnik( String ime, String prezime, String username, String password) {
+		super();
+		
+		Ime = ime;
+		Prezime = prezime;
+		Username = username;
+		Password = password;
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getIme() {
+		return Ime;
+	}
+	public void setIme(String ime) {
+		Ime = ime;
+	}
+	public String getPrezime() {
+		return Prezime;
+	}
+	public void setPrezime(String prezime) {
+		Prezime = prezime;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	
+}
