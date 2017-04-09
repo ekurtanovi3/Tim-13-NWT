f647b1d7e4d25be4b33533c4d1b7b11fce22e9d4
MerimaKlico <mklico1@etf.unsa.ba>
2017-04-09 14:30:27 +0200

    prepravljen pom.xml, application.properties, mainapplication

--- Film/MoviepediaSpring/src/main/java/etf/nwt/tim13/MoviepediaSpringApplication.java
+++ Film/MoviepediaSpring/src/main/java/etf/nwt/tim13/MoviepediaSpringApplication.java
@@ -2,8 +2,12 @@ package etf.nwt.tim13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MoviepediaSpringApplication {
 
 	public static void main(String[] args) {
