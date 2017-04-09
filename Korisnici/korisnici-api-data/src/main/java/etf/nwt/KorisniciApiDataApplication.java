140788794af7e9cd9481b1b6dbe0d242330efaa3
arzama <arza_1993@hotmail.com>
2017-04-09 14:44:59 +0200

    config service and eureka service

--- Korisnici/korisnici-api-data/src/main/java/etf/nwt/KorisniciApiDataApplication.java
+++ Korisnici/korisnici-api-data/src/main/java/etf/nwt/KorisniciApiDataApplication.java
@@ -2,8 +2,10 @@ package etf.nwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class KorisniciApiDataApplication {
 
 	public static void main(String[] args) {
